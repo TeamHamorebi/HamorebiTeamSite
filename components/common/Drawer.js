9c9be11694e67e00d1c7d920810c5d7893af84cd
araaiko <>
2022-07-26 18:14:58 +0900

    Create header, drawer and buttons

---
+++ components/common/Drawer.js
@@ -0,0 +1,87 @@
+import styles from "../../styles/components/common/_Drawer.module.scss";
+import Link from "next/link";
+import Button from "./Button";
+import React, { useState } from "react";
+
+export default function Drawer(props) {
+  const data = props.list; // Layout.jsで使用
+
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuFunction = () => {
+    setOpenMenu(!openMenu);
+  };
+
+  return (
+    <header className={styles.header}>
+      {/* inner */}
+      <div className={styles.inner}>
+        {/* header brand */}
+        <div className={styles.brand}>
+          <Link href="./">
+            <a className={styles.brandLink}>
+              <div className={styles.brandImgWrap}>
+                <img src="/img/common/hamorebi_logo_color.svg" alt="はもれび" />
+              </div>
+              <p className={styles.brandName}>
+                Hamorebi{" "}
+                <span className={styles.brandSubName}>ーはもれびー</span>
+              </p>
+            </a>
+          </Link>
+        </div>
+        {/* /header brand */}
+
+        {/* drawer button */}
+        <div
+          type="button"
+          className={`${styles.drawerBtn} ${
+            openMenu ? styles.isActive : undefined
+          }`}
+          onClick={() => menuFunction()}
+        >
+          <div className={styles.drawerBtnBarWrap}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        {/* /drawer button */}
+
+        {/* drawer navigation */}
+        <nav
+          className={`${styles.navigation} ${
+            openMenu ? styles.isOpen : undefined
+          }`}
+        >
+          {/* drawer items */}
+          <ul className={styles.drawerItems}>
+            {data.map((value, index) => {
+              return (
+                <li key={index} className={styles.drawerItem}>
+                  <Link href={`#${value.link}`}>
+                    <a className={styles.drawerItemLink}>{value.name}</a>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+          {/* /drawer items */}
+          <Button
+            styles={styles.drawerBtnWrap}
+            text={"お問い合わせをする"}
+            link={"#contact"}
+          ></Button>
+        </nav>
+        {/* /drawer navigation */}
+        {/* overlay */}
+        <div
+          className={`${styles.overlay} ${
+            openMenu ? styles.isOpen : undefined
+          }`}
+        ></div>
+        {/* /overlay */}
+      </div>
+      {/* /inner */}
+    </header>
+  );
+}
---
+++ components/common/Header.js
@@ -0,0 +1,50 @@
+import styles from "../../styles/components/common/_Header.module.scss";
+import Button from "./Button";
+import InnerBase from "./InnerBase";
+import Link from "next/link";
+
+export default function Header(props) {
+  const data = props.list; // Layout.jsで使用
+
+  return (
+    <header className={styles.header}>
+      <InnerBase>
+        {/* header wrap */}
+        <div className={styles.headerWrap}>
+          {/* header brand */}
+          <div className={styles.brandWrap}>
+            <h1 className={styles.brand}>
+              <img src="/img/common/hamorebi_logo_color.svg" alt="はもれび" />
+            </h1>
+          </div>
+          {/* /header brand */}
+
+          {/* header navigation */}
+          <nav className={styles.navigation}>
+            {/* nav items */}
+            <ul className={styles.navItems}>
+              {data.map((value, index) => {
+                return (
+                  <li key={index} className={styles.navItem}>
+                    <Link href={`#${value.link}`}>
+                      <a className={styles.navLink}>{value.name}</a>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+            {/* /nav items */}
+          </nav>
+          {/* /header navigation */}
+
+          <Button
+            styles={styles.headerBtnWrap}
+            text={"お問い合わせをする"}
+            link={"#contact"}
+          ></Button>
+        </div>
+        {/* /header wrap */}
+      </InnerBase>
+    </header>
+  );
+}
