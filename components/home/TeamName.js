ed86a9f37e447ef2c3d418234ec0f28982e57cf7
araaiko <>
2022-08-12 18:47:41 +0900

    create team name section

---
+++ components/home/TeamName.js
@@ -0,0 +1,26 @@
+import styles from "../../styles/components/home/_TeamName.module.scss";
+
+export default function TeamName() {
+  return (
+    <section className={styles.teamName}>
+      <div className={styles.textWrap}>
+        <p className={styles.text}>
+          チーム名の「Hamorebi  ーはもれびー」には
+          <span className={styles.textBoldface}>
+            太陽のように私たちは「自ら輝き」
+            <br />
+            光を受けた葉は「成長する」
+            <br />
+            みんなで奏でよう「ハーモニー」
+            <br />
+            葉漏れ日のように「道を切り開く」
+          </span>
+          <span className={styles.textBottom}>
+            というチームの思いが込められております
+          </span>
+        </p>
+
+      </div>
+    </section>
+  );
+}
