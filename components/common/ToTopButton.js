377b77b04cbe9ebbdd2af50052009eb78a8e2c4a
araaiko <>
2022-08-27 18:43:00 +0900

    Added scroll animation to the back to top button トップへ戻るボタンに、一定量スクロール後ふわっと表示するアニメーションを追加

--- components/common/ToTopButton.js
+++ components/common/ToTopButton.js
@@ -1,10 +1,27 @@
 import styles from "../../styles/components/common/_ToTopButton.module.scss";
 import Link from "next/link";
-// import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function ToTopButton() {
+  // 一定量スクロール後ふわっと表示
+  const [isVisible, setIsVisible] = useState(false);
+  const toggleVisibility = () => {
+    window.scrollY > 200 ? setIsVisible(true) : setIsVisible(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    // return () => {
+    //   window.removeEventListener("scroll", toggleVisibility);
+    // };
+  }, []);
+
   return (
-    <div className={styles.toTop}>
+    <div
+      className={`${styles.toTop} ${
+        isVisible ? styles.isFadeIn : styles.isFadeOut
+      }`}
+    >
       <Link href="#top">
         <a className={styles.toTopLink}>
           <div className={styles.toTopArrowWrap}>
