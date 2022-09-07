02a2b7fa15dd86cc8165f6923e815dbb756303f8
araaiko <araaiko.122@gmail.com>
2022-09-07 14:22:59 +0900

    fix OGP

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import Head from "next/head";
 import { useEffect } from "react";
 import { changeViewport } from "../lib/changeViewport";
 import { smoothScroll } from "../lib/smoothScroll";
-import Seo from "../components/common/Seo";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -16,15 +15,43 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <title>Web制作チーム Hamorebiーはもれびー</title>
+        <meta
+          name="description"
+          content={
+            "私達はWeb制作を承るフリーランスチームのHamorebi ーはもれびーです。Webサイト、ECサイト、ランディングページ、UIデザイン等の制作を行っております。日本各地のクリエーター達が、皆様のお悩みやお困りごとに寄り添って、解決のお手伝いをします"
+          }
+        />
+        <meta
+          property="og:url"
+          content={"https://hamorebi-team-site.netlify.app"}
+        />
+        <meta
+          property="og:title"
+          content={"Web制作チーム Hamorebiーはもれびー"}
+        />
+        <meta
+          property="og:site_name"
+          content={"Web制作チーム Hamorebiーはもれびー"}
+        />
+        <meta
+          property="og:description"
+          content={
+            "私達はWeb制作を承るフリーランスチームのHamorebi ーはもれびーです。Webサイト、ECサイト、ランディングページ、UIデザイン等の制作を行っております。日本各地のクリエーター達が、皆様のお悩みやお困りごとに寄り添って、解決のお手伝いをします"
+          }
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content={
+            "https://hamorebi-team-site.netlify.app/img/top/hamorebi_logo_color--big.png"
+          }
+        />
+        <meta property="og:image:width" content={804} />
+        <meta property="og:image:height" content={562} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <link rel="canonical" href={"https://hamorebi-team-site.netlify.app"} />
       </Head>
-      <Seo
-        pagePath={"https://hamorebi-team-site.netlify.app"}
-        pageImg={
-          "https://hamorebi-team-site.netlify.app/img/top/hamorebi_logo_color--big.png"
-        }
-        pageImgWidth={804}
-        pageImgHeight={562}
-      />
       <Component {...pageProps} />
     </>
   );
