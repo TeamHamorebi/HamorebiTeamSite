ca085387d64204f30624f51275dca7bc57e00b8b
araaiko <araaiko.122@gmail.com>
2022-09-09 11:15:58 +0900

    インデックス登録用metaタグ追加

--- pages/_app.js
+++ pages/_app.js
@@ -20,6 +20,11 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        {/* google search console */}
+        <meta
+          name="google-site-verification"
+          content="RtZ1y7CMsyKpE_iNqTT6oieFMk2iItCJXjD9y1H46QQ"
+        />
       </Head>
       <Component {...pageProps} />
     </>
